bc7adbec93424ed290dd2a6f806e1bf989a4e493
zhenqiang.wang <zhenqiang.wang@dynax-semi.com>
2023-07-26 10:06:34 +0800

    适配器模式

---
+++ sjms/src/com/sjms/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package com.sjms.adapter;
+
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+    public void playMp4(String fileName);
+}
---
+++ sjms/src/com/sjms/adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.sjms.adapter;
+
+public interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}

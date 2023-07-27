4e69430aba7c89a003c6829c5a71de0963ba9146
zhenqiang.wang <zhenqiang.wang@dynax-semi.com>
2023-07-27 11:50:02 +0800

    命令模式

---
+++ sjms/src/com/sjms/commandPattern/Order.java
@@ -0,0 +1,8 @@
+package com.sjms.commandPattern;
+
+/**
+ * 创建一个命令接口
+ */
+public interface Order {
+    void execute();
+}
---
+++ sjms/src/com/sjms/commandPattern/SellStock.java
@@ -0,0 +1,13 @@
+package com.sjms.commandPattern;
+
+public class SellStock implements Order {
+    private Stock abcStock;
+
+    public SellStock(Stock abcStock){
+        this.abcStock = abcStock;
+    }
+
+    public void execute() {
+        abcStock.sell();
+    }
+}

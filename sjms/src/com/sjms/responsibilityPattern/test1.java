c8e02989c3a2221166cc523c40992f88f0ec9d21
zhenqiang.wang <zhenqiang.wang@dynax-semi.com>
2023-07-27 11:28:46 +0800

    责任链模式

---
+++ sjms/src/com/sjms/responsibilityPattern/test1.java
@@ -0,0 +1,12 @@
+package com.sjms.responsibilityPattern;
+
+public class test1 {
+    public String a1 = "A1";
+    public String a2 = "A2";
+}
+
+class test{
+    public static void t(){
+
+    }
+}
---
+++ sjms/src/com/sjms/responsibilityPattern/test2.java
@@ -0,0 +1,7 @@
+package com.sjms.responsibilityPattern;
+
+public class test2 extends test1 {
+    public String a1 = "1A1";
+    public String a2 = "1A2";
+    public String a3 = "1A3";
+}

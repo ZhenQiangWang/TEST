fd16a742da678a8c5bbefbb32f1615c3f404502d
zhenqiang.wang <zhenqiang.wang@dynax-semi.com>
2023-08-03 17:01:21 +0800

    迭代器模式

--- pattern/src/com/pattern/IteratorPattern/IteratorPatternDemo.java
+++ pattern/src/com/pattern/IteratorPattern/IteratorPatternDemo.java
@@ -1,5 +1,10 @@
 package com.pattern.IteratorPattern;
 
+/**
+ * 迭代器模式（Iterator Pattern）是 Java 和 .Net 编程环境中非常常用的设计模式。这种模式用于顺序访问集合对象的元素，不需要知道集合对象的底层表示。
+ *
+ * 迭代器模式属于行为型模式。
+ */
 public class IteratorPatternDemo {
 
     public static void main(String[] args) {

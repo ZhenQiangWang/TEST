e4efd74b20ef3536bd779735fb2bc3231ee8591d
zhenqiang.wang <zhenqiang.wang@dynax-semi.com>
2023-08-03 16:01:39 +0800

    装饰器模式

---
+++ sjms/src/com/sjms/decoratorPattern/DecoratorPatternDemo.java
@@ -0,0 +1,28 @@
+package com.sjms.decoratorPattern;
+
+/**
+ * 装饰器模式（Decorator Pattern）允许向一个现有的对象添加新的功能，同时又不改变其结构。这种类型的设计模式属于结构型模式，它是作为现有的类的一个包装。
+ *
+ * 装饰器模式通过将对象包装在装饰器类中，以便动态地修改其行为。
+ *
+ * 这种模式创建了一个装饰类，用来包装原有的类，并在保持类方法签名完整性的前提下，提供了额外的功能。
+ */
+
+public class DecoratorPatternDemo {
+    public static void main(String[] args) {
+
+        Shape circle = new Circle();
+        ShapeDecorator redCircle = new RedShapeDecorator(new Circle());
+        ShapeDecorator redRectangle = new RedShapeDecorator(new Rectangle());
+//        Shape redCircle = new RedShapeDecorator(new Circle());
+//        Shape redRectangle = new RedShapeDecorator(new Rectangle());
+        System.out.println("Circle with normal border");
+        circle.draw();
+
+        System.out.println("\nCircle of red border");
+        redCircle.draw();
+
+        System.out.println("\nRectangle of red border");
+        redRectangle.draw();
+    }
+}
---
+++ sjms/src/com/sjms/decoratorPattern/Shape.java
@@ -0,0 +1,5 @@
+package com.sjms.decoratorPattern;
+
+public interface Shape {
+    void draw();
+}

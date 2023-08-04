10f43855a3ce17b6a1d55fa806aa798dd61cfdcb
zhenqiang.wang <zhenqiang.wang@dynax-semi.com>
2023-08-04 15:40:58 +0800

    状态模式

--- pattern/src/com/pattern/observerPattern/ObserverPatternDemo.java
+++ pattern/src/com/pattern/observerPattern/ObserverPatternDemo.java
@@ -1,5 +1,10 @@
 package com.pattern.observerPattern;
 
+/**
+ * 观察者模式是一种行为型设计模式，它定义了一种一对多的依赖关系，当一个对象的状态发生改变时，其所有依赖者都会收到通知并自动更新。
+ *
+ * 当对象间存在一对多关系时，则使用观察者模式（Observer Pattern）。比如，当一个对象被修改时，则会自动通知依赖它的对象。观察者模式属于行为型模式。
+ */
 public class ObserverPatternDemo {
     public static void main(String[] args) {
         Subject subject = new Subject();
---
+++ pattern/src/com/pattern/statePattern/State.java
@@ -0,0 +1,5 @@
+package com.pattern.statePattern;
+
+public interface State {
+    public void doAction(Context context);
+}

88e9afc487e9ef872387e347d741a138d6e47513
zhenqiang.wang <3099826153@qq.com>
2023-08-06 08:57:53 +0800

    模版模式

--- pattern/src/com/pattern/strategyPattern/StrategyPatternDemo.java
+++ pattern/src/com/pattern/strategyPattern/StrategyPatternDemo.java
@@ -1,5 +1,10 @@
 package com.pattern.strategyPattern;
 
+/**
+ * 在策略模式（Strategy Pattern）中一个类的行为或其算法可以在运行时更改。这种类型的设计模式属于行为型模式。
+ * 在策略模式定义了一系列算法或策略，并将每个算法封装在独立的类中，使得它们可以互相替换。通过使用策略模式，可以在运行时根据需要选择不同的算法，而不需要修改客户端代码。
+ * 在策略模式中，我们创建表示各种策略的对象和一个行为随着策略对象改变而改变的 context 对象。策略对象改变 context 对象的执行算法。
+ */
 public class StrategyPatternDemo {
     public static void main(String[] args) {
         Context context = new Context(new OperationAdd());

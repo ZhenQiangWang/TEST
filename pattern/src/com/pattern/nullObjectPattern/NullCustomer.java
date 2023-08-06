df42abe59fd25531ed9205a23352d066385d2066
zhenqiang.wang <3099826153@qq.com>
2023-08-06 08:48:19 +0800

    空对象模式

---
+++ pattern/src/com/pattern/nullObjectPattern/NullCustomer.java
@@ -0,0 +1,14 @@
+package com.pattern.nullObjectPattern;
+
+public class NullCustomer extends AbstractCustomer {
+
+    @Override
+    public String getName() {
+        return "Not Available in Customer Database";
+    }
+
+    @Override
+    public boolean isNil() {
+        return true;
+    }
+}
